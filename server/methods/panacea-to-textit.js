90415678d0ef77c8f0936f7891643aee3f913320
Marvin Arnold <marvinmarnold@gmail.com>
2016-07-16 12:51:46 +0300

    Channel in table.

--- server/methods/panacea-to-textit.js
+++ server/methods/panacea-to-textit.js
@@ -4,7 +4,6 @@ import { insertIncomingSms, sendSmsToPanacea } from '../../lib/sms.js';
 // Endpoint for Message Originating (MO), user to Panacea
 // GET request with to, from, content query parameters
 //
-
 const incomingEndpoint = Meteor.settings.SECRET_TOKEN + "/panacea-to-textit";
 Meteor.method(incomingEndpoint, (to, from, content) => {
   const incomingSmsId = insertIncomingSms({to, from, text})
