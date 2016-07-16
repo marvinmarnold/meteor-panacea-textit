769501a324067981bb71ebec6cfa1b824a2f6b82
Marvin Arnold <marvinmarnold@gmail.com>
2016-07-17 00:00:31 +0300

    Export Contacts as part of panacea-textit

--- common/main.js
+++ common/main.js
@@ -1,8 +1,10 @@
 import { Channels } from './collections/channels.js';
+import { Contacts } from './collections/contacts.js';
 import { LANGUAGES } from '../lib/constants.js';
 
 export {
   LANGUAGES,
 
-  Channels
+  Channels,
+  Contacts
 };
