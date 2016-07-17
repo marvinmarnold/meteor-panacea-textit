5704d807019ebc62c7b128576cd5d37fc83288f3
Marvin Arnold <marvinmarnold@gmail.com>
2016-07-17 17:09:05 +0300

    Forward to textit successfully

--- server/methods/panacea-to-textit.js
+++ server/methods/panacea-to-textit.js
@@ -1,22 +1,23 @@
 import { Meteor } from 'meteor/meteor';
-import { insertIncomingSms, sendSmsToPanacea } from '../../lib/sms.js';
+import { insertIncomingSms, sendSmsToTextIt } from '../../lib/sms.js';
 
 // Endpoint for Message Originating (MO), user to Panacea
 // GET request with to, from, content query parameters
 //
 const incomingEndpoint = Meteor.settings.SECRET_TOKEN + "/panacea-to-textit";
-Meteor.method(incomingEndpoint, (to, from, content) => {
-  const incomingSmsId = insertIncomingSms({to, from, text})
+Meteor.method(incomingEndpoint, (to, from, text) => {
+  const incomingSmsId = insertIncomingSms(to, from, text)
   sendSmsToTextIt(incomingSmsId);
 
   return 1;
 }, {
   httpMethod: "get",
   getArgsFromRequest(request) {
-    console.log(request);
+    console.log('panacea-to-textit');
+    // console.log(request);
 
     var q = request.query;
-
-    return [ q.to, q.from, q.content ];
+    console.log(q);
+    return [ q.to, q.from, q.text ];
   }
 })
